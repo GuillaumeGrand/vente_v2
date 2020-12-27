114517cb186a571bf86bf99a4d8df8121f69b8c9
guillaume grand <guillaume.grand80@orange.fr>
2020-12-27 22:35:17 +0100

    set url for dpmain prod

--- app/controllers/checkouts_controller.rb
+++ app/controllers/checkouts_controller.rb
@@ -20,39 +20,12 @@ class CheckoutsController < ApplicationController
             destination: cart[0].store.trader.stripe_account,
           },
         },
-        success_url: "base_url",
-        cancel_url: "/stores/1",
+        success_url: base_url,
+        cancel_url:  base_url,
       })
-      CreateOrder.new(cart,user_id).call
+      # CreateOrder.new(cart,user_id).call
       render json: session
 
   end
-
-  # def solo_checkout
-  #   user_id = current_user.id
-  #   cart = FetchCart.new(params[:store_id],user_id).call
-
-  #     session = Stripe::Checkout::Session.create({
-  #       payment_method_types: ['card'],
-  #       line_items: [{
-  #         name: cart[0].store.name,
-  #         amount: Total.new(cart).call,
-  #         currency: 'eur',
-  #         quantity: 1,
-  #       }],
-  #       payment_intent_data: {
-  #         application_fee_amount: 0,
-  #         transfer_data: {
-  #           destination: cart[0].store.trader.stripe_account,
-  #         },
-  #       },
-  #       success_url: "/",
-  #       cancel_url: "/stores/1",
-  #     })
-  #     CreateOrder.new(cart,user_id).call
-  #     render json: session
-
-  # end
-
 
 end
