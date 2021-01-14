8a1a34ef0c060d0e0d0e20d4a844fba65fbc1b1d
guillaume grand <guillaume.grand80@orange.fr>
2021-01-14 21:19:46 +0100

    html/css orders

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2021_01_09_191805) do
+ActiveRecord::Schema.define(version: 2021_01_14_143739) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -72,6 +72,7 @@ ActiveRecord::Schema.define(version: 2021_01_09_191805) do
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
     t.integer "stock"
+    t.string "reference"
     t.index ["store_id"], name: "index_products_on_store_id"
   end
 
